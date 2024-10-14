b507f9a7ff6f606cd43661521a92f2722cd2ea83
syafiq7974 <syafiqburhanuddin66@gmail.com>
2024-10-14 11:14:41 +0700

    Menyelesaikan    tugas    praktikum    Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,230 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(BookCatalogApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class BookCatalogApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Katalog Buku',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.indigo,
+        scaffoldBackgroundColor: Colors.grey[100],
+        appBarTheme: AppBarTheme(elevation: 2, color: Colors.indigoAccent),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/detail': (context) => DetailPage(),
+        '/about': (context) => AboutPage(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// Halaman Home: Menampilkan daftar buku
+class HomePage extends StatelessWidget {
+  final List<Book> books = [
+    Book(
+      'Dunia Sophie',
+      'Jostein Gaarder',
+      'Dunia Sophie adalah sebuah novel filsafat yang diterbitkan pada tahun 1991 oleh penulis Norwegia, Jostein Gaarder. Cerita ini mengikuti Sophie Amundsen, seorang gadis berusia 14 tahun yang mulai menerima surat-surat misterius yang memperkenalkannya pada sejarah filsafat. Melalui perjalanan waktu, Sophie mempelajari ajaran-ajaran dari para filsuf besar seperti Socrates, Plato, Aristoteles, dan banyak lainnya. Novel ini menggabungkan fiksi dengan gagasan-gagasan filsafat dalam bentuk yang menarik dan mendidik, menjadikannya bacaan yang mengasyikkan sekaligus membuka wawasan tentang pemikiran filosofis Barat.',
+    ),
+    Book(
+      'Laskar Pelangi',
+      'Andrea Hirata',
+      'sebuah novel karya Andrea Hirata yang diterbitkan pada tahun 2005. Novel ini mengisahkan perjuangan sekelompok anak sekolah dari desa miskin di Belitung, Indonesia, yang meskipun dalam keterbatasan, tetap gigih untuk meraih pendidikan. Dibimbing oleh dua guru yang berdedikasi, anak-anak ini menjalin persahabatan erat dan mengatasi berbagai tantangan seperti kemiskinan, kurangnya fasilitas, dan pandangan masyarakat. Novel ini menekankan tema ketekunan, persahabatan, dan pentingnya pendidikan. Laskar Pelangi menjadi salah satu novel terpopuler di Indonesia dan telah diadaptasi ke dalam film, teater, dan berbagai bentuk lainnya.',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: Text(
+          'Katalog Buku',
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.info_outline),
+            onPressed: () => Navigator.pushNamed(context, '/about'),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Cari buku...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              onChanged: (value) {
+                // Logic for search filtering (optional enhancement)
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: books.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 4,
+                  margin: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: ListTile(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.indigoAccent,
+                      child: Icon(
+                        Icons.book,
+                        color: Colors.white,
+                        size: 24,
+                      ),
+                    ),
+                    title: Text(
+                      books[index].title,
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    subtitle: Text(
+                      books[index].author,
+                      style: TextStyle(fontStyle: FontStyle.italic),
+                    ),
+                    trailing: Icon(Icons.arrow_forward_ios),
+                    onTap: () {
+                      Navigator.pushNamed(
+                        context,
+                        '/detail',
+                        arguments: books[index],
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => Navigator.pushNamed(context, '/about'),
+        backgroundColor: Colors.indigoAccent,
+        icon: Icon(Icons.info_outline),
+        label: Text('Tentang'),
+      ),
     );
   }
+}
+
+// Halaman Detail Buku: Menampilkan informasi detail buku
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Book book = ModalRoute.of(context)!.settings.arguments as Book;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Detail Buku'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Hero(
+                  tag: book.title, // Add hero animation
+                  child: Icon(
+                    Icons.book,
+                    size: 100,
+                    color: Colors.indigoAccent,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                book.title,
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'oleh ${book.author}',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.grey[700]),
+              ),
+              SizedBox(height: 16),
+              Text(
+                book.description,
+                style: TextStyle(fontSize: 16, height: 1.5),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Halaman Tentang Aplikasi: Menampilkan informasi tentang aplikasi
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Tentang Aplikasi')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.book, size: 100, color: Colors.indigoAccent),
+              SizedBox(height: 20),
+              Text(
+                'Katalog Buku',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Versi 1.0.0',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Aplikasi ini dikembangkan oleh Syafiq Burhanuddin untuk menampilkan katalog buku sederhana dengan navigasi interaktif.',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 20),
+              Divider(),
+              Text(
+                'Hak Cipta © 2024 Syafiq Burhanuddin. Semua hak dilindungi.',
+                style: TextStyle(fontSize: 12, color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Model Buku
+class Book {
+  final String title;
+  final String author;
+  final String description;
+
+  Book(this.title, this.author, this.description);
 }
